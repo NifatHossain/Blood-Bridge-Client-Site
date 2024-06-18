5f50e4368be261f33161502b65df2fc8bc90470f
NifatHossain <nifathossain1480@gmail.com>
2024-06-18 18:46:47 +0600

    created donation request form

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,32 +1,32 @@
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
 import useVolunteer from "../hooks/useVolunteer";
-import { FaBlogger, FaGlobe, FaHandsHelping, FaHome } from "react-icons/fa";
-import { BiDonateBlood } from "react-icons/bi";
-import { MdAddBox } from "react-icons/md";
+import { FaBlogger, FaGlobe, FaHandsHelping, FaHome, FaNotesMedical, FaUser, FaUsers } from "react-icons/fa";
+import { MdAddBox} from "react-icons/md";
 
 
 const Dashboard = () => {
-    const [isAdmin]= useAdmin()
+    const [isAdmin]=useAdmin(); 
     const [isVolunteer]=useVolunteer()
     return (
-        <div className="flex">
-            <div className="w-60 min-h-screen bg-orange-400">
+        <div className="flex roboto">
+            <div className="w-60 min-h-screen bg-rose-400">
                 <ul className="menu gap-4">
+                    <li><div className=" text-2xl bg-white"><div className="flex flex-col items-center"><div><span className="text-red-500">Blood</span> Bridge</div><p className="text-xs font-light text-red-500">Connecting Doners, Saving Lives</p></div></div></li>
                     {
                         isAdmin? <>
                             <li><NavLink to='/dashboard/adminhome'><FaGlobe></FaGlobe> Admin Home</NavLink></li>
-                            <li><NavLink to='/dashboard/donars'><BiDonateBlood /> All Doners</NavLink></li>
-                            <li><NavLink to='/dashboard/allrequests'><MdAddBox /> All Donation Requests</NavLink></li>
+                            <li><NavLink to='/dashboard/alldonars'><FaUsers /> All Doners</NavLink></li>
+                            <li><NavLink to='/dashboard/allrequests'><MdAddBox/> All Donation Requests</NavLink></li>
                             <li><NavLink to='/dashboard/contentsmanagement'><FaBlogger /> Contents Management</NavLink></li>
                         </>:isVolunteer?<>
                             <li><NavLink to='/dashboard/volunteerhome'><FaHandsHelping /> volunteer Home</NavLink></li>
                             <li><NavLink to='/dashboard/allrequests'><MdAddBox /> All Donation Requests</NavLink></li>
                             <li><NavLink to='/dashboard/contentsmanagement'><FaBlogger /> Contents Management</NavLink></li>
                         </>:<>
-                            <li><NavLink to='/dashboard/donarhome'>My Dashboard</NavLink></li>
-                            <li><NavLink to='/dashboard/myrequests'>My Donation Requests</NavLink></li>
-                            <li><NavLink to='/dashboard/createrequest'>Create Donation Request</NavLink></li>
+                            <li><NavLink to='/dashboard/donarhome'><FaUser></FaUser> My Dashboard</NavLink></li>
+                            <li><NavLink to='/dashboard/myrequests'><MdAddBox /> My Donation Requests</NavLink></li>
+                            <li><NavLink to='/dashboard/createrequest'><FaNotesMedical /> Create Donation Request</NavLink></li>
                         </>
                     }
                     <div className="divider"></div>
--- src/pages/donarPages/CreateRequest.jsx
+++ src/pages/donarPages/CreateRequest.jsx
@@ -1,9 +1,126 @@
-
+import Lottie from "lottie-react";
+import donationAnimation from "../../../public/bloodDonation.json"
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAuth from "../../hooks/useAuth";
 
 const CreateRequest = () => {
+    const [districts, setDistricts]=useState([])
+    const [upazilas, setUpazilas]=useState([])
+    const {user}=useAuth()
+    const axiosPublic=useAxiosPublic()
+    const navigate= useNavigate()
+    useEffect(()=>{
+        fetch('../../public/districts.json')
+    .then(res=>res.json())
+    .then(data=>setDistricts(data[2].data))
+    },[])
+    useEffect(()=>{
+        fetch('../../public/upazilas.json')
+    .then(res=>res.json())
+    .then(data=>setUpazilas(data[2].data))
+    },[])
+
+    
+    // console.log(upazilas[0])
+    const { register,handleSubmit, formState: { errors } } = useForm();
+    
+    const onSubmit = async(data) => {
+        data.userName=user.displayName;
+        data.userEmail=user.email;
+        console.log(data);
+        //axiosPublic.post('/adduser',userInfo)
+        //         .then((result)=>{
+        //                 if(result.data.insertedId){
+        //                 Swal.fire({
+        //                     position: "top-end",
+        //                     icon: "success",
+        //                     title: "Successfully Updated user Info",
+        //                     showConfirmButton: false,
+        //                     timer: 1500
+        //                 });
+        //                 navigate('/')
+        //                 }
+        //         })
+                
+        //     })
+        //     .catch((error)=>{
+        //         console.log(error)
+        //     })
+        // })
+        // .catch(error=>{
+        //     console.log(error.message)
+        // })
+
+    }
     return (
-        <div>
-            <h2>create request</h2>
+        <div className="py-5 bg-rose-50">
+            <div className="flex justify-center">
+                <h2 className="text-center text-2xl w-[70%] px-6 font-semibold p-3 bg-rose-300 rounded-md mb-4">Create new Request</h2>
+            </div>
+            <div className="flex gap-5">
+                <Lottie className=" w-1/2 flex-1 hidden" animationData={donationAnimation} />
+                <div className="flex flex-col items-center flex-1  w-full">
+                    <div className=" text-2xl"><div className="flex flex-col items-center"><div><span className="text-red-500">Blood</span> Bridge</div><p className="text-xs font-light text-red-500">Connecting Doners, Saving Lives</p></div></div>
+                    <form className="flex flex-col gap-3 w-[60%] mt-7" onSubmit={handleSubmit(onSubmit)}>
+                        <input type="text" className="border-2 rounded-sm p-2" disabled defaultValue={user.displayName} placeholder="name" {...register("userName")} />
+                        <input type="email" className="border-2 rounded-sm p-2" disabled defaultValue={user.email} placeholder="email" {...register("userEmail")} />
+
+                        <p className="text-slate-400">Enter Recipient Name: </p>
+                        <input type="text" className="border-2 rounded-sm p-2" placeholder="Recipient Name" {...register("recipientName", {required: true})} />
+                        {errors.name && <p className="text-red-500">Enter Recipient name</p>}
+                        <p className="text-slate-400">Enter Recipient Blood Group: </p>
+                        <select className="border-2 rounded-sm p-2" {...register("blood_group", { required: 'required field '})}>
+                            <option value="">Enter Blood Group</option>
+                            <option value="A+">A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                            <option value="O+">O+</option>
+                            <option value="O-">O-</option>
+                        </select>
+                        {errors.blood_group && <p className="text-red-500">blood group required</p>}
+                        <p className="text-slate-400">Enter Recipient District: </p>
+                        <select className="border-2 p-2 rounded-sm" {...register("district", { required: true })}>
+                            <option value="Dhaka">Select District</option>
+                            {
+                                districts.map(district=><option key={district.id} value={district.name}>{district.name}</option>)
+                            }
+                        </select>
+                        {errors.district && <p className="text-red-500">Secect District</p>}
+                        <p className="text-slate-400">Enter Recipient Upzilla: </p>
+                        <select className="border-2 p-2 rounded-sm pt-0" {...register("upzilla", { required: true })}>
+                            <option value="Dhaka">Select Upzilla</option>
+                            {
+                                upazilas.map(upazila=><option key={upazila.id} value={upazila.name}>{upazila.name}</option>)
+                            }
+                        </select>
+                        {errors.upazila && <p className="text-red-500">Secect Upazila</p>}
+                        <p className="text-slate-400">Enter Hospital Name </p>
+                        <input type="text" className="border-2 rounded-sm p-2" placeholder="hospitalName" {...register("hospitalName", {required: true})} />
+                        {errors.hospitalName && <p className="text-red-500">Enter Hospital name</p>}
+                        <p className="text-slate-400">Enter Full Address </p>
+                        <input type="text" className="border-2 rounded-sm p-2" placeholder="fullAddress" {...register("fullAddress", {required: true})} />
+                        {errors.fullAddress && <p className="text-red-500">Enter Full Address</p>}
+                        <p className="text-slate-400">Enter Donation Date </p>
+                        <input type="date" className="border-2 rounded-sm p-2" placeholder="donationDate" {...register("donationDate", {required: true})} />
+                        {errors.donationDate && <p className="text-red-500">Enter donation Date</p>}
+                        <p className="text-slate-400">Enter Donation Time </p>
+                        <input type="time" className="border-2 rounded-sm p-2" placeholder="donationTime" {...register("donationTime", {required: true})} />
+                        {errors.donationTime && <p className="text-red-500">Enter donation Time</p>}
+                        <p className="text-slate-400">Enter Patient&apos;s condition </p>
+                        <input type="text" className="border-2 rounded-sm p-2" placeholder="patientDetails" {...register("patientDetails", {required: true})} />
+                        {errors.patientDetails && <p className="text-red-500">Enter patient&apos;s Details</p>}
+                        
+                        <input type="submit"className="btn" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
