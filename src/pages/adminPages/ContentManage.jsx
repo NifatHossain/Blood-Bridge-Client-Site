d08a8d1fef3f43e5e4a57ab4af3d5715c4668577
NifatHossain <nifathossain1480@gmail.com>
2024-06-20 23:03:37 +0600

    Content Management page done for Admin

--- src/pages/adminPages/ContentManage.jsx
+++ src/pages/adminPages/ContentManage.jsx
@@ -1,14 +1,65 @@
 import { useState } from "react";
 import { MdCreateNewFolder } from "react-icons/md";
-import { NavLink, Outlet } from "react-router-dom";
-
+import { Link, NavLink, Outlet } from "react-router-dom";
+import useAllArticles from "../../hooks/useAllArticles";
+import Lottie from "lottie-react";
+import noDataAnimation from "../../../public/noDataFound.json"
+import useAdmin from "../../hooks/useAdmin";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ContentManage = () => {
     const[hide,setHide]=useState(false)
+    const [articles,refetch]=useAllArticles()
+    const axiosSecure= useAxiosSecure();
+    const [isAdmin]=useAdmin()
     const handleHide=()=>{
         setHide(!hide)
     }
+    const handlePublish=(id,status)=>{
+        axiosSecure.patch(`modifyarticlestatus?id=${id}&status=${status}`)
+        .then(Result=>{
+            if(Result.data.modifiedCount>0){
+                refetch()
+                Swal.fire({
+                title: "Success",
+                text: "Article status changed",
+                icon: "success"
+                });
+                // navigate('/dashboard/myrequests')
+                
+            }
+        })
+    }
+    const handleDelete=(id)=>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+            
+                axiosSecure.delete(`/deletearticle/${id}`)
+                .then(res=>{
+                    if(res.data.deletedCount>0){
+                        refetch();
+                        Swal.fire({
+                        title: "Deleted!",
+                        text: "Article has been deleted.",
+                        icon: "success"
+                        });
+                    }
+                })
+            }
+        });
+    }
     return (
+        
         <div>
             <div className="py-5 bg-teal-50">
                 <div className="flex justify-center gap-5">
@@ -18,7 +69,41 @@ const ContentManage = () => {
                     }
                 </div>
                 <div className={hide&&"hidden bg-teal-50" }>
-                    <h2>All previous contents</h2>
+                    {
+                        (!(articles.length>0))?<>
+                            <div className="flex justify-center items-center">
+                                <Lottie className=" w-1/2 " animationData={noDataAnimation} />
+                            </div>
+                        </>:<div  className="ml-4 grid grid-cols-3 gap-7">
+                            {
+                                articles.map((article)=><div key={article._id}>
+                                    <div className="border-2 rounded-lg">
+                                        <div>
+                                            <img className="w-full" src={article.image} alt="" />
+                                        </div>
+                                        <div className="p-3 bg-white">
+                                            <div className="flex justify-around">
+                                                <p className="text-slate-400">Author: {article.authorName}</p>
+                                                <p className="text-slate-400">Status: {article.status}</p>
+                                            </div>
+                                            <h2 className="text-lg font-semibold">{article.title}</h2>
+                                            <span>{(article.article).slice(0,100)}</span>
+                                            <Link to={`/articleDetails/${article._id}`}><span className="btn btn-xs">Read More</span></Link>
+                                            <hr className="my-3" />
+                                            <div className="flex gap-4">
+                                                {
+                                                    ((article.status==='draft') && isAdmin)?<button onClick={()=>handlePublish(article._id,'published')} className="btn bg-teal-400 text-white">Publish</button>:((article.status==='published') && isAdmin) &&<button onClick={()=>handlePublish(article._id,'draft')} className="btn bg-teal-400 text-white">Unpublish</button>
+                                                }
+                                                {
+                                                    isAdmin && <button onClick={()=>handleDelete(article._id)} className="btn bg-rose-400 text-white">Delete</button>
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
+                        </div>
+                    }
                 </div>
                 
             </div>
