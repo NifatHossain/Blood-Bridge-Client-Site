57d2ce4fa1e71e27c80414ae3abd3d7a84a610a2
NifatHossain <nifathossain1480@gmail.com>
2024-06-21 11:46:37 +0600

    Created recent campaings section

--- src/pages/generalPages/Home.jsx
+++ src/pages/generalPages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./homeComponents/Banner";
 import Footer from "./homeComponents/Footer";
+import RecentCampains from "./homeComponents/RecentCampains";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <RecentCampains></RecentCampains>
             <Footer></Footer>
         </div>
     );
---
+++ src/pages/generalPages/homeComponents/RecentCampains.jsx
@@ -0,0 +1,41 @@
+import { BsCalendarDate } from "react-icons/bs";
+import { FaRegClock } from "react-icons/fa";
+import { SiGooglemaps } from "react-icons/si";
+
+
+const RecentCampains = () => {
+    return (
+        <div className="flex items-center bg-teal-50 px-4 py-4">
+            <div className="space-y-3 max-w-2xl">
+                <h1 className="text-5xl font-semibold"><span className="border-b-2 border-red-500">OUR</span> CAMPAINS</h1>
+                <p className="font-medium text-lg">All over the country we have arranged around hundred donation campaigns and visit other venues on various occasions.</p>
+                <button className="cursor-pointer font-medium bg-red-500 text-white p-5">LOAD ALL CAMPAINS</button>
+            </div>
+            <div className="card max-w-2xl  rounded-none card-side bg-base-100 shadow-xl">
+                <figure className="h-full"><img src="https://templates.bwlthemes.com/blood_donation/v_2/images/event_1.jpg" alt="Movie"/></figure>
+                <div className="card-body">
+                    <div className="flex items-center gap-4">
+                        <BsCalendarDate className="text-red-500" />
+                        <p>24 February, 2024</p>
+                    </div>
+                    <h2 className="card-title">Open Blood Donation</h2>
+                    <p>Join our Open Blood Donation Campaign! Your donation can save lives. Come, donate blood, and be a hero. Every drop counts. Let's make a difference together!</p>
+                    <div className="card-actions">
+                        <div className="flex gap-7">
+                            <div className="flex gap-1 items-center">
+                                <FaRegClock className="text-red-500" />
+                                <p>09.00-3.30</p>
+                            </div>
+                            <div className="flex gap-1 items-center">
+                                <SiGooglemaps className="text-red-500" />
+                                <p>North South University</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecentCampains;
